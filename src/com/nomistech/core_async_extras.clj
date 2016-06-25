31fdb9735a82bbd69afc7878f50e99e2c0d028ed
Simon Katz <sk@nomistech.com>
2016-06-25 15:44:59 +0100

    Create library project from template

---
+++ src/com/nomistech/core_async_extras.clj
@@ -0,0 +1,6 @@
+(ns com.nomistech.core-async-extras)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/com/nomistech/core_async_extras_test.clj
@@ -0,0 +1,7 @@
+(ns com.nomistech.core-async-extras-test
+  (:require [clojure.test :refer :all]
+            [com.nomistech.core-async-extras :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
