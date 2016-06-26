a484a75802e243a0e0bc74b2fa6b6db0f2618050
Simon Katz <sk@nomistech.com>
2016-06-26 10:32:52 +0100

    Test that `stoppable-go-loop` calls fun immediately

--- test/com/nomistech/core_async_extras_test.clj
+++ test/com/nomistech/core_async_extras_test.clj
@@ -1,20 +1,27 @@
 (ns com.nomistech.core-async-extras-test
   (:require [clj-time.core :as t]
             [clojure.core.async :as a]
+            [clj-time.coerce :as c]
             [com.nomistech.core-async-extras :refer :all]
             [midje.sweet :refer :all]))
 
 (fact "`stoppable-go-loop` works"
+  ;; TODO: Is this kind of test with timing ok? How else to do it?
   (let [frequency-ms    100
         time-to-run-for 350
         acc-atom        (atom [])
+        start-time      (t/now)
         control-ch      (stoppable-go-loop #(swap! acc-atom conj (t/now))
                                            frequency-ms)]
     (Thread/sleep time-to-run-for)
     (a/>!! control-ch :stop)
     (let [ts @acc-atom
           diffs (map (comp t/in-millis t/interval) ts (rest ts))]
-      (fact
+      (fact "The function was first called immediately"
+        (c/to-long (first ts))
+        => (roughly (c/to-long start-time)
+                    10))
+      (fact "The function was called at intervals of `frequency-ms`, until `time-to-run-for` had elapsed"
         diffs
         => (let [expected-n-iterations (int (/ time-to-run-for
                                                frequency-ms))]
