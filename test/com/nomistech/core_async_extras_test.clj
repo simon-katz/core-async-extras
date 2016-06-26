a346f93ecd99e668c6d4398dc2d9e73f8ca5218d
Simon Katz <sk@nomistech.com>
2016-06-26 10:52:06 +0100

    Add given/when/then to test

--- test/com/nomistech/core_async_extras_test.clj
+++ test/com/nomistech/core_async_extras_test.clj
@@ -7,23 +7,28 @@
 
 (fact "`stoppable-go-loop` should work"
   ;; TODO: Is this kind of test with timing ok? How else to do it?
+  ;; Given
   (let [frequency-ms    100
         time-to-run-for 350
         acc-atom        (atom [])
-        start-time      (t/now)
-        control-ch      (stoppable-go-loop #(swap! acc-atom conj (t/now))
-                                           frequency-ms)]
-    (Thread/sleep time-to-run-for)
-    (a/>!! control-ch :stop)
-    (let [ts @acc-atom
-          diffs (map (comp t/in-millis t/interval) ts (rest ts))]
-      (fact "should first call the function immediately"
-        (c/to-long (first ts))
-        => (roughly (c/to-long start-time)
-                    10))
-      (fact "should call the function at intervals of `frequency-ms`, until `time-to-run-for` has elapsed"
-        diffs
-        => (let [expected-n-iterations (int (/ time-to-run-for
-                                               frequency-ms))]
-             (just (repeat expected-n-iterations
-                           (roughly frequency-ms 10))))))))
+        start-time      (t/now)]
+    ;; When
+    (let [control-ch (stoppable-go-loop #(swap! acc-atom conj (t/now))
+                                        frequency-ms)]
+      (Thread/sleep time-to-run-for)
+      (a/>!! control-ch :stop)
+      ;; Then
+      (let [ts @acc-atom
+            diffs (map (comp t/in-millis t/interval)
+                       ts
+                       (rest ts))]
+        (fact "should first call the function immediately"
+          (c/to-long (first ts))
+          => (roughly (c/to-long start-time)
+                      10))
+        (fact "should call the function at intervals of `frequency-ms`, until `time-to-run-for` has elapsed"
+          diffs
+          => (let [expected-n-iterations (int (/ time-to-run-for
+                                                 frequency-ms))]
+               (just (repeat expected-n-iterations
+                             (roughly frequency-ms 10)))))))))
